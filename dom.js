4f678e36048bf619c63073968f3e11d79f3184c8
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-03-07 16:08:24 +0530

    Changes made

--- dom.js
+++ dom.js
@@ -1,17 +1,24 @@
 const button = document.querySelector(".data");
-const nav = document.querySelector(".nav-link")
 
-button.addEventListener("mouseover", () => {
+button.addEventListener("click", () => {
   button.classList.toggle("active");
-  console.log("Button was clicked!");
+  console.log("lol");
 }, false);
 
+const hola = document.querySelectorAll(".hola");
+hola.forEach(item => item.addEventListener("mouseover", () => {
+  item.classList.toggle("active");
+}),
+  false);
 
-nav.addEventListener("mouseover", () => {
-  nav.classList.toggle("active");
-  console.log("The link was change");
-}, false);
+  const hooray = document.querySelectorAll("hr");
+hooray.forEach(lol => lol.addEventListener("mouseover", () => {
+  lol.classList.toggle("active");
+}),
+  false);
 
-// nav.addEventListener("mouseover", () => {
-//   nav.classList.lastElementChild.toggle("active");
-// }, false);
+const nav= document.querySelectorAll(".nav-link"); 
+nav.forEach(haha => haha.addEventListener("click", () => {
+  haha.classList.toggle("active");
+}),
+  false);
