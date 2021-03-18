0fb21ad8f20206e54f5d71cbe4c329e51d604085
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-03-18 22:31:44 +0530

    Final changes

--- dom.js
+++ dom.js
@@ -4,10 +4,10 @@ let button = document.querySelector(".data");
   button.classList.toggle("active");
 }), false;
 
-document.querySelectorAll("hr").forEach(lol => lol.addEventListener("mouseover", () => {
-  lol.classList.toggle("active");
-}),
-  false);
+// // document.querySelectorAll("hr").forEach(lol => lol.addEventListener("mouseover", () => {
+//   lol.classList.toggle("active");
+// // }),
+//   false);
 
 document.querySelectorAll(".contact").forEach(notokay => notokay.addEventListener("mouseover", () => {
   notokay.classList.toggle("active");
@@ -18,3 +18,10 @@ document.querySelectorAll(".nav-link").forEach(haha => haha.addEventListener("cl
   haha.classList.toggle("active");
 }),
   false);
+
+document.querySelectorAll(".card").forEach(Element => Element.addEventListener("click", () => {
+  Element.classList.toggle("active");
+}),
+  false);
+
+    
