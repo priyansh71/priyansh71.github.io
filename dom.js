154d50cde5d08d55151431472187cc52ab2f5758
priyansh71 <f20201388@goa.bits-pilani.ac.in>
2021-04-01 17:43:54 +0530

    added smooth transition

--- dom.js
+++ dom.js
@@ -1,18 +1,8 @@
 
 let button = document.querySelector(".data");
-  button.addEventListener("click", () => {
+button.addEventListener("click", () => {
   button.classList.toggle("active");
 }), false;
-
-// // document.querySelectorAll("hr").forEach(lol => lol.addEventListener("mouseover", () => {
-//   lol.classList.toggle("active");
-// // }),
-//   false);
-
-document.querySelectorAll(".contact").forEach(notokay => notokay.addEventListener("mouseover", () => {
-  notokay.classList.toggle("active");
-}),
-  false);
 
 document.querySelectorAll(".nav-link").forEach(haha => haha.addEventListener("click", () => {
   haha.classList.toggle("active");
@@ -24,4 +14,3 @@ document.querySelectorAll(".card").forEach(Element => Element.addEventListener("
 }),
   false);
 
-    
